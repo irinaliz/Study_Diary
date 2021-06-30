7ba1e8ebeb3e4724aa267f66cae43da3daa976b5
irinaliz <dlqhals3@naver.com>
2021-06-30 14:41:58 +0900

    day9

---
+++ src/com/irinaliz/study_diary/b06_jun/day9/Student.java
@@ -0,0 +1,35 @@
+package com.irinaliz.study_diary.b06_jun.day9;
+
+public class Student {
+	private String name;
+	private int age;
+	private int height;
+	private int weight;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public int getHeight() {
+		return height;
+	}
+	public void setHeight(int height) {
+		this.height = height;
+	}
+	public int getWeight() {
+		return weight;
+	}
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}
+	
+
+}
---
+++ src/com/irinaliz/study_diary/b06_jun/day9/Student_Main.java
@@ -0,0 +1,31 @@
+package com.irinaliz.study_diary.b06_jun.day9;
+class Person2{
+	int age;
+	public String name;
+	protected int height;
+	private int weight;
+	
+}
+public class Student_Main extends Person2{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Student leebomin = new Student();
+		leebomin.setAge(25);
+		leebomin.setName("이보민");
+		leebomin.setHeight(172);
+		leebomin.setWeight(85);
+		
+		System.out.println(leebomin.getAge());
+		
+		
+	}
+	public void dataset() {
+		age = 1;
+		name = "qq";
+		height = 170;
+//		weight = 12;
+	}
+
+}
