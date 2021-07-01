e744e84171df50faea170d6f78f3d8dbc115516d
irinaliz <dlqhals3@naver.com>
2021-07-01 19:28:28 +0900

    Day 11 DateFormat

--- src/com/irinaliz/study_diary/b06_jun/day11/quiz/Main.java
+++ src/com/irinaliz/study_diary/b06_jun/day11/quiz/Main.java
@@ -1,6 +1,8 @@
 package com.irinaliz.study_diary.b06_jun.day11.quiz;
 
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 
 public class Main {
     public static void main(String[] args){
@@ -9,5 +11,13 @@ public class Main {
         Q1.q4("2021-04-25" , "2021-06-25");
 
         Q1.q2();
+
+        Date date = new Date();
+        System.out.println(date);
+        SimpleDateFormat df =
+                new SimpleDateFormat("yyyy년 MM월 dd일 hh시 mm분 ss초 E요일 입니다.");
+        System.out.println(df.format(date));
+
+
     }
 }
